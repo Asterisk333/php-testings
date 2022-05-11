a1733e00c1c8ed029dcbfb6fde2f8ab84801eca6
Astersik <spk02@bluewin.ch>
2022-05-11 19:30:21 +0200

    ---General--- changes for bootstrap ---Var---

--- utility/header.php
+++ utility/header.php
@@ -7,7 +7,8 @@
     <title>Test-page</title>
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <link rel="stylesheet" href="./style/variables.scss" type="text/scss">
+    <link rel="stylesheet" href="../scss/_variables.scss" type="text/scss">
+    <link rel="stylesheet" href="../scss/header.scss" type="text/scss">
 </head>
 <body>
     <div class="container">
