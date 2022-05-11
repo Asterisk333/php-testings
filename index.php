a1196e033368ca25319f991c2c2fd04c8c44a4f7
Astersik <spk02@bluewin.ch>
2022-05-11 19:11:32 +0200

    ---General--- changes for bootstrap ---Var---

--- index.php
+++ index.php
@@ -1,21 +1,9 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!--<meta http-equiv="Refresh" content="30">-->
-    <title>Test-page</title>
-</head>
-<header>
     <?php include './utility/header.php'; ?>
-</header>
-<body>
+
     <main>
         <h1>Test Page</h1>
 
     </main>
-</body>
-<footer>
+
     <?php include './utility/footer.php'; ?>
-</footer>
-</html>
+
