8b8dfe61307fb6c77bc64d6f816d58ef9c97d164
Asterisk333 <105341307+Asterisk333@users.noreply.github.com>
2022-05-11 17:16:42 +0200

    ---General--- changeing to bootstrap4 ---Var---

--- index.php
+++ index.php
@@ -1,14 +1,10 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <link rel="stylesheet" href="./style/header.css" type="text/css">
-    <link rel="stylesheet" href="./style/footer.css" type="text/css">
-    <link rel="stylesheet" href="./style/index.css" type="text/css">
     <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <meta http-equiv="Refresh" content="30">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!--<meta http-equiv="Refresh" content="30">-->
     <title>Test-page</title>
-    <script src="./src/jquery.min.js"></script>
 </head>
 <header>
     <?php include './utility/header.php'; ?>
