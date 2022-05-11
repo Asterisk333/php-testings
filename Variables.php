fc83c66843b7810606f5f6e9ab5e69368bab84a6
Asterisk333 <105341307+Asterisk333@users.noreply.github.com>
2022-05-11 10:19:17 +0200

    ---General--- add function dominus
    ---Var---

--- Variables.php
+++ Variables.php
@@ -2,7 +2,13 @@
 //this is a file to test values and github commits
 
 function doadd($nbra,$nbrb){
-    echo $nbra+$nbrb;
+    echo $nbra + $nbrb;
 }
 
 doadd(1,3);
+
+function dominus($nbra,$nbrb){
+    echo $nbra - $nbrb;
+}
+
+dominus(1,5);
