1cec710b7ef87969bfe6ebd2133c1dd71a661325
Astersik <spk02@bluewin.ch>
2022-05-11 19:06:51 +0200

    ---General--- changeing to bootstrap4 ---Var---

--- utility/header.php
+++ utility/header.php
@@ -1,23 +1,9 @@
-<<<<<<< HEAD
 <head>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link rel="stylesheet" href="./style/variables.scss" type="text/scss">
 </head>
-=======
->>>>>>> origin/master
-<table>
-    <tr>
-        <!--Main-->
-        <td>
-            <a href="../index.php"><img src="" alt="Index" class=navigation>
-        </td>
-        <!-- Filler-->
-        <td id="filler">
-        </td>
-        <!--Search (no function so far)-->
-        <td>
-            <a><img src="" alt="server" class=navigation>
-        </td>
-    </tr>
-</table>
+<div class="container">
+
+    <a>test</a>
+</div>
 
