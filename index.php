c0c675ff3bdfcebf242e17b45ee1813a09892d3a
Asterisk333 <105341307+Asterisk333@users.noreply.github.com>
2022-05-11 11:43:40 +0200

    ---General--- add style add src add utility
    ---Var---

--- index.php
+++ index.php
@@ -1,13 +1,26 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>my test page</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" href="./css/header.css" type="text/css">
+    <link rel="stylesheet" href="./css/footer.css" type="text/css">
+    <link rel="stylesheet" href="./css/index.css" type="text/css">
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="Refresh" content="30">
+    <title>Test-page</title>
+    <script src="./src/jquery.min.js"></script>
 </head>
 <body>
     <h1>Test Page</h1>
-
+    <table>
+    <tr>
+        <td>
+            yeet
+        </td>
+        <td>
+            yeet2
+        </td>
+    </tr>
+    </table>
 </body>
 </html>
