0db86862273901616a99a49ab6f96d177ee890d9
Asterisk333 <105341307+Asterisk333@users.noreply.github.com>
2022-05-11 11:47:17 +0200

    ---General--- fixes
    ---Var---

--- index.php
+++ index.php
@@ -1,26 +1,25 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <link rel="stylesheet" href="./css/header.css" type="text/css">
-    <link rel="stylesheet" href="./css/footer.css" type="text/css">
-    <link rel="stylesheet" href="./css/index.css" type="text/css">
+    <link rel="stylesheet" href="./style/header.css" type="text/css">
+    <link rel="stylesheet" href="./style/footer.css" type="text/css">
+    <link rel="stylesheet" href="./style/index.css" type="text/css">
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta http-equiv="Refresh" content="30">
     <title>Test-page</title>
     <script src="./src/jquery.min.js"></script>
 </head>
+<header>
+    <?php include './utility/header.php'; ?>
+</header>
 <body>
-    <h1>Test Page</h1>
-    <table>
-    <tr>
-        <td>
-            yeet
-        </td>
-        <td>
-            yeet2
-        </td>
-    </tr>
-    </table>
+    <main>
+        <h1>Test Page</h1>
+        
+    </main>
 </body>
+<footer>
+    <?php include './utility/footer.php'; ?>
+</footer>
 </html>
